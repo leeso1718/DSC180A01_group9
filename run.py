70618ef38a5f53135cea4063ef8a2b5f8c367449
Sohyun Lee <49351511+leeso1718@users.noreply.github.com>
2020-11-25 23:09:20 -0800

    Update run.py for method7

--- run.py
+++ run.py
@@ -7,12 +7,14 @@ sys.path.insert(0, 'src/data')
 sys.path.insert(0, 'src/analysis')
 sys.path.insert(0, 'src/model')
 sys.path.insert(0, 'src/util')
+sys.path.insert(0, 'test/')
 
 from src.util import download_annotations
 from src.util import run_demo
 from src.model import model
 from src.util import coco_dataloader
 from src.util import coco_dict
+from test import test_model
 
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
@@ -150,8 +152,35 @@ def main(targets):
         
         model.test_model(net, testloader, device)
         
-        
+        elif 'method7' in targets:
+            with open('test/test-data-params.json') as fh:
+                method7_config = json.load(fh)
+                
+            with open('config/model-params.json') as fh:
+                model_cfg = json.load(fh)
+                
+            #load in data
+            inputs, labels = test_model.load_testdata(method7_config['train_inputs'],method7_config['train_labels'])
+            #transforms
+            transform = transforms.Compose([transfroms.Resize([240, 240]), transforms.ToTensor()])
             
+            # change to gpu
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            print('Computing as: {}'.format(device))
+            
+            net = model.Net()
+            net = net.to(device)
+
+            #run the model
+            test_model.run_model(
+                input_tensors = inputs,
+                label_tensors = labels,
+                net = net,
+                epochs = model_cfg['epochs'],
+                lr = model_cfg['lr'],
+                momentum = model_cfg['momentum'],
+                modelDir = m7['modelDir'],
+                device = device)
 if __name__ == '__main__':
     targets = sys.argv[1:]
     main(targets)
